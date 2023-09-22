85cc2adb56cffae9c954087ae50be726848240c8
sreenesh <sreenshreddy52@gmail.com>
2023-09-22 21:43:47 +0530

    ..

--- sih/accounts/forms.py
+++ sih/accounts/forms.py
@@ -5,15 +5,21 @@ from django.contrib.auth.models import User
 class SignUpForm(UserCreationForm):
     email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
     studentid=forms.CharField(required=True,widget=forms.TextInput())
+    is_student = forms.CharField(
+        initial=True,
+        required=False)
     class Meta:
         model = User
+        
         fields = ('username', 'email', 'studentid','password1','password2')
 
 class college_Regform(UserCreationForm):
     email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
     collegeid=forms.CharField(required=True,widget=forms.TextInput())
     location = forms.CharField(max_length=200,required=True,widget=forms.TextInput())
+    
 
     class Meta:
         model = User
+        is_student = False
         fields=('username','email','location','password1','password2')
