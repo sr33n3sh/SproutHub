ef0d39ff470844653cf89c3ea3b59c43773280ba
SumanR1602 <suman160204@gmail.com>
2023-09-22 20:59:28 +0530

    clg part

--- sih/sprouthub/admin.py
+++ sih/sprouthub/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Institute,ProjectInstituteStudent,AnyStudent,Projects
+from .models import Institute,ProjectInstituteStudent,AnyStudent,Projects,Project
 
 admin.site.register(Institute)
 admin.site.register(ProjectInstituteStudent)
 admin.site.register(AnyStudent)
 admin.site.register(Projects)
+# admin.site.register(CreateProject)
+admin.site.register(Project)
